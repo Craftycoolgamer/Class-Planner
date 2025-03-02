01ab27971b9e0f04b271a5cfc4880c445ec9c805
Quinn Prisbrey <Craftycoolgamer@gmail.com>
2025-03-02 15:13:48 -0700

    Started project

---
+++ TermsApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TermsApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
